7d13f8da5ab8baf9019b4ad036bce6532b4802eb
Dmitry Borzov <tihoutrom@gmail.com>
2015-11-08 12:49:17 +0300

    Proofread for basic blights

--- dynamic-programming/longest-common-substring_test.go
+++ dynamic-programming/longest-common-substring_test.go
@@ -1,10 +1,13 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestBasic(t *testing.T) {
 	P := "what a beautiful morning, there is something in the air"
 	Q := "I love the smell of napalm in the morning, it is just the woman's things you wont get it"
-    output := LCS([]byte(P), []byte(Q))
-	fmt.Printf("lo and behold: %#v \n", output)	
+	output := LCS(P, Q)
+	fmt.Printf("lo and behold: %#v \n", output)
 }
