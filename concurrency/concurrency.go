98e4c89b4bb669c52ee578e2383e3986c4849e74
Dmitry Borzov <tihoutrom@gmail.com>
2015-12-02 18:32:22 +0300

    Showcase some channel communication

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -6,18 +6,30 @@ import (
 	"time"
 )
 
-func pirate(name string) {
+func pirate(name string, out chan string) {
 	cnt := 0
 	for {
 		cnt++
-		time.Sleep((200 + time.Duration(rand.Intn(1300))) * time.Millisecond)
-		fmt.Printf("%v says: heyo for %vth time! \n", name, cnt)
+		time.Sleep((time.Duration(rand.Intn(1300))) * time.Millisecond)
+		if cnt > 3 {
+			out <- fmt.Sprintf("%v is tired of this shit and leaves\n", name)
+			return
+		}
+		res := fmt.Sprintf("%v says: heyo for %vth time! \n", name, cnt)
+		out <- res
 	}
 }
 
 func main() {
-	for i := 65; i < 90; i++ {
-		go pirate(fmt.Sprintf("Pirate%v", string(rune(i))))
+	log := make(chan string)
+	for i := 65; i < 68; i++ {
+		go pirate(fmt.Sprintf("Pirate %v", string(rune(i))), log)
 	}
-	time.Sleep(5000 * time.Millisecond)
+
+	go func() {
+		for l := range log {
+			fmt.Printf(l)
+		}
+	}()
+	time.Sleep(10000 * time.Millisecond)
 }
